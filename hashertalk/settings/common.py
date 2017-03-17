37daaa7d4ab59bfa78a349d73cd6f623a1e2ee95
Sripathi Krishnan <Sripathi.Krishnan@gmail.com>
2017-03-17 13:38:47 +0530

    Adding django bootstrap and prettying up forms

--- hashertalk/settings/common.py
+++ hashertalk/settings/common.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'hashertalk.discussions',
 ]
 
